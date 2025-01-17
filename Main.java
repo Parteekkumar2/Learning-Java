cab6d31b37a62d7035d085728e659169f336bb9c
Parteek <parteekkumar2021@gmail.com>
2025-01-17 21:18:56 +0530

    modify

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Welcome to Online IDE!! Happy Coding :)");
+        System.out.println("hello world");
     }
 }
