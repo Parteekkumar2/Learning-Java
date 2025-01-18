3ef3f3fb50a992ac656c10238125347009e7da0d
Parteek <parteekkumar2021@gmail.com>
2025-01-18 10:51:44 +0530

    modify

--- input.java
+++ input.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class input{
-    public static void main(String[]args){
-         Scanner scn=new Scanner(System.in);
+    public static void main(String[] args){
+         Scanner scn =new Scanner(System.in);
          int x=scn.nextInt();
          int y=scn.nextInt();
          System.out.println(x+y);
