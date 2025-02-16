5a8118a71a60cc1df1fa7b05fd709ccbf83c0a1a
Parteek <parteekkumar2021@gmail.com>
2025-02-16 23:06:22 +0530

    print factorial of number

---
+++ for loop/factorial.java
@@ -0,0 +1,10 @@
+public class factorial {
+public static void main(String[] args){
+    int x=3;
+   int fact=1;
+    for(int i=1;i<=x;i++){
+        fact=fact*i;
+    }
+    System.out.println(fact);
+}    
+}
