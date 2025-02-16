b1c3cd0a2bd2283350a1e0609ee891ae8ea59bc4
Parteek <parteekkumar2021@gmail.com>
2025-02-16 21:16:14 +0530

    if else statement

---
+++ marks.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class marks {
+public static void main(String[] args){
+   Scanner scn=new Scanner(System.in);
+   int x=scn.nextInt();
+   if(x>90){
+    System.out.println("excellent");
+   }else if(x>80 && x<=90){
+    System.out.println("good");
+   }else if(x>70 && x<=80){
+    System.out.println("average");
+   }else if(x>60 && x<=70){
+    System.out.println("below average");
+   }else if(x<60){
+    System.out.println("fail");
+   }
+   scn.close();
+}
+}
