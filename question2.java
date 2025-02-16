ebf2c6a1b6ffe8b51d79568fdd997be91956b73b
Parteek <parteekkumar2021@gmail.com>
2025-02-16 22:02:10 +0530

    while loop question

---
+++ question2.java
@@ -0,0 +1,12 @@
+public class question2 {
+        public static void main(String[] args){
+            int i=0;
+            while(i<=5){
+                System.out.println("Parteek");
+                i++;
+            }
+        }
+        
+    }
+    
+
