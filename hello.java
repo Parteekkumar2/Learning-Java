25a9374cc2d725ba8c96083729b8ed5b7f27e0f8
Parteek <parteekkumar2021@gmail.com>
2025-02-16 11:08:22 +0530

    Add input method

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class hello {
+        public static void main(String[] args){
+             Scanner scn=new Scanner(System.in);
+         int x = scn.nextInt();
+         int y = scn.nextInt();
+            System.out.println(x+y);
+        }
+    }
