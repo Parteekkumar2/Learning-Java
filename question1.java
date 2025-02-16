10a9e82a303e7f7aa808cf4575b7f7980a724f8c
Parteek <parteekkumar2021@gmail.com>
2025-02-16 21:37:25 +0530

    a b c greater

--- question1.java
+++ question1.java
@@ -5,8 +5,15 @@ public class question1 {
         int a=scn.nextInt();
         int b=scn.nextInt();
         int c=scn.nextInt();
-
+       if(a>=b && a>=c){
+        System.out.println(a + "is greater");
+       }else if(b>=a && b>=c){
+        System.out.println( b + "is greater ");
+       }else{
+        System.out.println(  c + "is greater");
+       }
+       scn.close();
     }
-    scn.close();
+    
     
 }
