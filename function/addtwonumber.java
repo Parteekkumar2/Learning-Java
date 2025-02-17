691319ceba8def149d77cded38d881bef4bac60f
Parteek <parteekkumar2021@gmail.com>
2025-02-17 21:59:29 +0530

    sum of two number using  function

---
+++ function/addtwonumber.java
@@ -0,0 +1,12 @@
+package function;
+
+public class addtwonumber {
+    public static void main(String[] args){
+        int x=sum(10,20);
+        System.out.println(x);
+        }  
+        
+        public static int sum(int a, int b){
+        return a+b;
+        } 
+}
