06c4e18d2f717998c96fc3f882917ddb51378dc0
Parteek <parteekkumar2021@gmail.com>
2025-02-21 20:19:08 +0530

    print greater than itself

---
+++ Arrays/greaterthanitselfarray.java
@@ -0,0 +1,47 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class greaterthanitselfarray {
+     public static void main(String[] args){
+        System.out.print("enter the size of arra : ");
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+
+        System.out.print("enter the element  of array: ");
+        int[] arr=new int[n];
+        for(int i=0;i<=n-1;i++){
+            arr[i]=scn.nextInt();
+        }
+        
+        System.out.print("Ans : ");
+        int ans=countgreater(arr);
+        System.out.println(ans);
+
+        
+        for(int i=0;i<=n-1;i++){
+            System.out.println(arr[i]);
+          }
+        
+
+        scn.close();
+    }
+
+    public static int countgreater(int[] arr){
+        int n=arr.length;
+           int max=arr[0];
+           for(int i=0;i<=n-1;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+           }
+           int maxcount=0;
+           for(int i=0;i<=n-1;i++){
+            if(arr[i]==max){
+                maxcount++;
+            }
+           }
+
+           return n-maxcount;
+    }
+}
