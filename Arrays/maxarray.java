ad4533afc66e2afea64c82e3be0e775b1be58c98
Parteek <parteekkumar2021@gmail.com>
2025-02-22 11:03:38 +0530

    print max array

---
+++ Arrays/maxarray.java
@@ -0,0 +1,42 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class maxarray {
+    public static void main(String[] args){
+        System.out.print("enter the size of arra : ");
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+
+        System.out.print("enter the element  of array: ");
+        int[] arr=new int[n];
+        for(int i=0;i<=n-1;i++){
+            arr[i]=scn.nextInt();
+        }
+        
+        System.out.print("Ans : ");
+        int ans=countgreater(arr);
+        System.out.print(ans);
+
+        scn.close();
+    }
+
+    public static int countgreater(int[] arr){
+        int n=arr.length;
+           int max=arr[0];
+           for(int i=0;i<=n-1;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+           }
+           return max;
+        //    int maxcount=0;
+        //    for(int i=0;i<=n-1;i++){
+        //     if(arr[i]==max){
+        //         maxcount++;
+        //     }
+        //    }
+
+        //    return n-maxcount;
+    }
+}
