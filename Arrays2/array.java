6ed9418ffe39c449568730b44c9cd26ba8132bd3
Parteek <parteekkumar2021@gmail.com>
2025-02-22 14:32:44 +0530

    print row wise in 2d array

---
+++ Arrays2/array.java
@@ -0,0 +1,23 @@
+package Arrays2;
+public class array {
+    public static void main(String[] args){
+        int[][] arr={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        rowwise(arr);
+    }
+
+    public static void rowwise(int[][] arr){
+        int n=arr.length;
+        int m=arr[0].length;
+        for(int i=0;i<=n-1;i++){
+            for(int j=0;j<=m-1;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
