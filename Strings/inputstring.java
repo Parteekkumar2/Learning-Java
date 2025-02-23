4384db176e32e8dc9d7649d4718cc2d1500f227f
Parteek <parteekkumar2021@gmail.com>
2025-02-23 10:47:36 +0530

    input String

---
+++ Strings/inputstring.java
@@ -0,0 +1,10 @@
+package Strings;
+import java.util.*;
+public class inputstring {
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        char ch=scn.nextLine().charAt(0);
+        System.out.println(ch);
+        scn.close();
+    }
+}
